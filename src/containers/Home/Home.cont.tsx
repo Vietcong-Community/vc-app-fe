4b9f4466a70041d001af14a29479c7017bd4f919
Vojtech Polcar <polcar.vojtech@gmail.com>
2024-10-27 17:10:03 +0100

    [VC-8] Global notifications provider

--- src/containers/Home/Home.cont.tsx
+++ src/containers/Home/Home.cont.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import { Select, Typography } from 'antd';
+import { Button, Select, Space, Typography } from 'antd';
 import { FormattedMessage } from 'react-intl';
 
 import { useMaps } from '../../api/hooks/enums/api';
+import { useNotifications } from '../../hooks/NotificationsHook';
+import { NotificationType } from '../../providers/NotificationsProvider/enums';
 
 import { messages } from './messages';
 
 export const HomeCont: React.FC = () => {
   const { data } = useMaps();
-  console.log(data);
+  const { showNotification } = useNotifications();
+
   return (
     <>
       HOME PAGE VJETKONGA
@@ -22,6 +25,33 @@ export const HomeCont: React.FC = () => {
         options={data?.map((item) => ({ value: item.id, label: <span>{item.name}</span> })) ?? []}
         style={{ margin: '0 auto', width: '10rem' }}
       />
+      <Space>
+        <Button onClick={() => showNotification('Uspech bez dalsiho popisku')}>Success</Button>
+        <Button
+          onClick={() =>
+            showNotification(
+              'Informacni notifikace',
+              'Pri volani v kodu je ctvrty argument cislo 7, ktere urcuje, za jak dlouho se zavru',
+              NotificationType.INFO,
+              7,
+            )
+          }
+        >
+          Info
+        </Button>
+        <Button
+          onClick={() =>
+            showNotification(
+              'Vystrazna notifikace',
+              'Ahoj, ja jsem ale pekna vystrazna notifikace',
+              NotificationType.WARNING,
+            )
+          }
+        >
+          Warning
+        </Button>
+        <Button onClick={() => showNotification('Chybka', 'Chybova notifikace', NotificationType.ERROR)}>Error</Button>
+      </Space>
     </>
   );
 };

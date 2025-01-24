74840464140113edba487e9ea10b609646475d73
Vojtěch Polcar <polcar.vojtech@gmail.com>
2025-01-24 14:14:12 +0100

    [NOISSUE] User team management endpoints

--- src/api/hooks/teams/endpoints.tsx
+++ src/api/hooks/teams/endpoints.tsx
@@ -1,4 +1,7 @@
 export enum TeamEndpoints {
-  TEAM_BY_ID = '/team/{id}',
-  TEAM_PLAYERS = '/team/{id}/users',
+  TEAM_BY_ID = '/teams/{id}',
+  TEAM_PLAYERS = '/teams/{id}/users',
+  REQUEST_TO_JOIN_TEAM = '/teams/{teamId}/join',
+  APPROVE_JOIN_REQUEST = '/teams/{teamId}/user/{userId}/approve',
+  REJECT_JOIN_REQUEST = '/teams/{teamId}/user/{userId}/reject',
 }

f6b680288ae6c37179e1d6b7b731a4d650caa4ac
Vojtěch Polcar <polcar.vojtech@gmail.com>
2025-01-29 16:52:17 +0100

    Accept match challenge messages fix

--- src/containers/League/AcceptMatchChallenge/messages.ts
+++ src/containers/League/AcceptMatchChallenge/messages.ts
@@ -2,47 +2,47 @@ import { defineMessages } from 'react-intl';
 
 export const messages = defineMessages({
   title: {
-    id: 'app.containers.League.CreateMatch.title',
+    id: 'app.containers.League.AcceptMatchChallenge.title',
     defaultMessage: 'Potvrzení výzvy k zápasu',
   },
   acceptSuccess: {
-    id: 'app.containers.League.CreateMatch.createSuccess',
+    id: 'app.containers.League.AcceptMatchChallenge.createSuccess',
     defaultMessage: 'Zápas byl úspěšně přijat, ať vyhraje ten ukempenější.',
   },
   rejectSuccess: {
-    id: 'app.containers.League.CreateMatch.rejectSuccess',
+    id: 'app.containers.League.AcceptMatchChallenge.rejectSuccess',
     defaultMessage: 'Zápas byl úspěšně odmítnut, jste unavený?.',
   },
   failedNotification: {
-    id: 'app.containers.League.CreateMatch.failedNotification',
+    id: 'app.containers.League.AcceptMatchChallenge.failedNotification',
     defaultMessage: 'Něco se pokazilo, zkuste to prosím později',
   },
   mapLabel: {
-    id: 'app.containers.League.CreateMatch.mapLabel',
+    id: 'app.containers.League.AcceptMatchChallenge.mapLabel',
     defaultMessage: 'Mapa',
   },
   submitButton: {
-    id: 'app.containers.League.CreateMatch.submitButton',
+    id: 'app.containers.League.AcceptMatchChallenge.submitButton',
     defaultMessage: 'Potvrdit',
   },
   cancelButton: {
-    id: 'app.containers.League.CreateMatch.cancelButton',
+    id: 'app.containers.League.AcceptMatchChallenge.cancelButton',
     defaultMessage: 'Odmítnout',
   },
   leaguesBreadcrumb: {
-    id: 'app.containers.League.CreateMatch.leaguesBreadcrumb',
+    id: 'app.containers.League.AcceptMatchChallenge.leaguesBreadcrumb',
     defaultMessage: 'Ligy a sezóny',
   },
   seasonDetailBreadcrumb: {
-    id: 'app.containers.League.CreateMatch.seasonDetailBreadcrumb',
+    id: 'app.containers.League.AcceptMatchChallenge.seasonDetailBreadcrumb',
     defaultMessage: 'Sezóna',
   },
   matchDetailBreadcrumb: {
-    id: 'app.containers.League.CreateMatch.matchDetailBreadcrumb',
+    id: 'app.containers.League.AcceptMatchChallenge.matchDetailBreadcrumb',
     defaultMessage: 'Zápas',
   },
   challengeBreadcrumb: {
-    id: 'app.containers.League.CreateMatch.challengeBreadcrumb',
+    id: 'app.containers.League.AcceptMatchChallenge.challengeBreadcrumb',
     defaultMessage: 'Výzva',
   },
 });

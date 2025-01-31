4ab8e75ea853283fb1ada7ae1a1fceaa86515bc5
bascci <chotetickymartin@gmail.com>
2025-02-01 00:00:31 +0100

    Temporary about us

--- src/containers/AboutUs/messages.ts
+++ src/containers/AboutUs/messages.ts
@@ -3,6 +3,14 @@ import { defineMessages } from 'react-intl';
 export const messages = defineMessages({
   title: {
     id: 'app.containers.AboutUs.title',
-    defaultMessage: 'O nás',
+    defaultMessage: 'O NÁS',
+  },
+  description: {
+    id: 'app.containers.AboutUs.description',
+    defaultMessage:
+      'Víme, že bys o nás rád věděl více, ale momentálně pracujeme na obsahu s vyšší prioritou. ' +
+      'Nezoufej! Brzy se jistě dozvíš plno zajímavých věcí. ' +
+      'Mezitím můžeš zavítat na <liga>detail ligy</liga> a podívat se, jak si vede tvůj oblíbený tým. ' +
+      'Nebo si můžeš zjistit informace o blížícím se <mcrvc>Mistrovství ČR</mcrvc>.',
   },
 });

4ca8ec269e7c6dd6576c7018f0b3a243953878cc
bascci <chotetickymartin@gmail.com>
2025-02-02 22:58:08 +0100

    StatistikyLink

--- src/containers/Statistics/Statistics.cont.tsx
+++ src/containers/Statistics/Statistics.cont.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 
+import { TrophyOutlined } from '@ant-design/icons';
 import { FormattedMessage } from 'react-intl';
 
 import { Gap } from '../../components/Gap/Gap';
+import { ContentLayout } from '../../components/Layouts/ContentLayout/ContentLayout';
+import { H1 } from '../../components/Titles/H1/H1';
+import { EXTERNAL_LINKS } from '../../constants/externalLinks';
 
 import { messages } from './messages';
+
+import * as S from './Statistics.style';
 
 export const StatisticsCont: React.FC = () => {
   return (
     <>
-      <h1>
-        <FormattedMessage {...messages.title} />
-      </h1>
-      <Gap defaultHeight={32} />
-      <FormattedMessage {...messages.tba} />
-      <Gap defaultHeight={96} />
+      <ContentLayout breadcrumbItems={[{ key: 'bc-mcrvc', title: <FormattedMessage {...messages.title} /> }]}>
+        <S.Container>
+          <H1>
+            <FormattedMessage {...messages.title} />
+          </H1>
+          <S.Content>
+            <FormattedMessage {...messages.tba} />
+            <Gap defaultHeight={5} />
+            <S.StatsLink>
+              <FormattedMessage {...messages.tbaA} />
+              <a href={EXTERNAL_LINKS.STATS_BY_ILE} target="_blank">
+                <TrophyOutlined />
+              </a>
+            </S.StatsLink>
+            <Gap defaultHeight={96} />
+          </S.Content>
+        </S.Container>
+      </ContentLayout>
     </>
   );
 };
